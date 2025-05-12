ac3f4c162740f451a750a7a0961dea5d4fd2e4a2
mariaquintero6 <aleja.qc12@gmail.com>
2025-05-12 07:03:27 -0500

    estructuras

--- estructura.dart
+++ estructura.dart
@@ -1,5 +1,5 @@
 void main (){
-  listExample();
+  mapExample();
 }
 
 void listExample(){
@@ -9,9 +9,45 @@ void listExample(){
   numbers[0] = "Uno modificado";
   numbers.add("Siete");
   numbers.addAll(numbers2);
-  numbers.removeAt(0);
-  numbers.remove("Siete");
-  numbers.insert(0, "Cero");
-  numbers.clear();
+  numbers.sort();
+  // numbers.removeAt(0);
+  // numbers.remove("Siete");
+  // numbers.insert(0, "Cero");
+  // numbers.clear();
   print(numbers);
+
+  for (var i = 0; i < numbers.length; i++){
+    print(numbers[i]);
+  }
+}
+
+void setExample(){
+  Set<String> numbers = {"uno", "dos", "tres"};
+  var numbers2 = {"dos", "cinco", "seis"};
+  numbers.add("siete");
+  numbers.addAll(numbers2);
+  // numbers.remove("uno");
+  // numbers.removeWhere((element) => element == "siete");
+
+  for (var i = 0; i < numbers.length; i++){
+    print(numbers.elementAt(i));
+  }
+  print(numbers);
+}
+
+void mapExample(){
+  Map<String, int> numbers = {
+    "uno": 1,
+    "dos": 2,
+    "tres": 3
+  };
+  numbers["cuatro"] = 4;
+  numbers["uno"] = 10;
+  // numbers.remove("dos");
+  numbers.removeWhere((key, value) => key == "uno");
+  print(numbers);
+
+  for (var i = 0; i < numbers.length; i++) {
+    print(numbers.entries.elementAt(i)); 
+  }
 }
